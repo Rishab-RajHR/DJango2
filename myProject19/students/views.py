d42273f40d4c8ee13de1c9763b1f008cf4a32e83
Rishab Raj <rr0948555@gmail.com>
2026-04-27 13:32:41 +0530

    Project

--- myProject19/students/views.py
+++ myProject19/students/views.py
@@ -1,11 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from .forms import StudentForm
+from .models import Student
+
+from django.shortcuts import render, get_object_or_404
+from .forms import StudentForm
+from .models import Student
 
 def student_create(request):
-    form = StudentForm()
     if request.method == 'POST':
         form = StudentForm(request.POST)
         if form.is_valid():
             form.save()
             return render(request, 'student_success.html')
-        return render(request, 'student_form.html', {'form': form()})
+    else:
+        form = StudentForm()
+
+    return render(request, 'student_form.html', {'form': form})
+    
+def student_list(request):
+    students = Student.objects.all()
+    return render(request, 'student_list.html', {'students': students})
+
+def student_detail(request, pk):
+    student = get_object_or_404(Student, pk=pk)
+    return render(request, 'student_detail.html', {'student': student})
+
+
