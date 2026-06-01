e3bea6cbcdfda6b43e03f415ac11db34a1a4dff7
Rishab Raj <rr0948555@gmail.com>
2026-06-01 23:10:08 +0530

    Cache File Based

---
+++ myProject36/blog/views.py
@@ -0,0 +1,17 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from .models import UserList
+from django.views.decorators.cache import cache_page
+from django.core.cache import cache
+
+@cache_page(30) # Cache the view for 30 seconds
+def users_list(request):
+    print("Fetching user list from database")
+    users = UserList.objects.all()
+    return render(request, 'users.html', {'users': users})
+  
+def clear_catch(request):
+    cache.clear()
+    return HttpResponse("All Cache Cleared")
+  
+
