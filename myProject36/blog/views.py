47d6a325b23142086e6c77880143ceee43f38f3a
Rishab Raj <rr0948555@gmail.com>
2026-06-02 19:20:05 +0530

    File based Caache

--- myProject36/blog/views.py
+++ myProject36/blog/views.py
@@ -4,9 +4,13 @@ from .models import UserList
 from django.views.decorators.cache import cache_page
 from django.core.cache import cache
 
-@cache_page(30) # Cache the view for 30 seconds
+# @cache_page(30) # Cache the view for 30 seconds
+# def users_list(request):
+#     print("Fetching user list from database")
+#     users = UserList.objects.all()
+#     return render(request, 'users.html', {'users': users})
+
 def users_list(request):
-    print("Fetching user list from database")
     users = UserList.objects.all()
     return render(request, 'users.html', {'users': users})
   
